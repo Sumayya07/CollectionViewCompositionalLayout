8f72fca97bf2fa8cd9e29514f1bd95cfc366c0a0
sumayya siddiqui <sumayyasiddiqui7173@gmail.com>
2023-03-10 22:46:31 +0530

    Collection View Compositional Layout

--- CollectionViewCompositionalLayout/ViewController.swift
+++ CollectionViewCompositionalLayout/ViewController.swift
@@ -16,27 +16,9 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        collectionView.collectionViewLayout = createLayout()
         setupLayout()
     }
-    
-    private func createLayout() -> UICollectionViewCompositionalLayout {
-        let item = CompositionalLayout.createItem(width: .fractionalWidth(0.02), height: .fractionalHeight(0.02), spacing: 1)
         
-        let fullItem = CompositionalLayout.createItem(width: .absolute(200), height: .absolute(200), spacing: 1)
-        let verticalGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(0.5), height: .fractionalHeight(1), item: fullItem, count: 2)
-        
-        let horizontalGroup = CompositionalLayout.createGroup(alignment: .horizontal, width: .fractionalWidth(1), height: .fractionalHeight(0.6), items: [item, verticalGroup])
-        
-        let mainItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .fractionalHeight(0.4), spacing: 1)
-        let mainGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(1), height: .fractionalHeight(0.5), items: [mainItem, horizontalGroup])
-        
-        let section = NSCollectionLayoutSection(group: mainGroup)
-        
-        // return
-        return UICollectionViewCompositionalLayout(section: section)
-    }
-    
     private func layout1() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .fractionalWidth(0.25))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -62,16 +44,22 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         }
     
     private func layout3() -> NSCollectionLayoutSection {
+        
+        let nestedItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/2)))
+        nestedItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let nestedGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/2), heightDimension: .fractionalHeight(1)), subitems: [nestedItem])
+        
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
         let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.4))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-                let section = NSCollectionLayoutSection(group: group)
-                section.orthogonalScrollingBehavior = .none
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, nestedGroup])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .none
                 
-                return section
-            }
+        return section
+}
     
     func setupLayout() {
         let layout = UICollectionViewCompositionalLayout { sectionIndex, env in
