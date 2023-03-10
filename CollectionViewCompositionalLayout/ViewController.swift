9f98caca378b03504ecb0878274d327d47f5bd72
sumayya siddiqui <sumayyasiddiqui7173@gmail.com>
2023-03-10 20:11:32 +0530

    Collection View Compositional Layout

--- CollectionViewCompositionalLayout/ViewController.swift
+++ CollectionViewCompositionalLayout/ViewController.swift
@@ -16,13 +16,14 @@ class ViewController: UIViewController, UICollectionViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.collectionViewLayout = createLayout()
+//        collectionView.collectionViewLayout = createLayout()
+        setupLayout()
     }
     
     private func createLayout() -> UICollectionViewCompositionalLayout {
-        let item = CompositionalLayout.createItem(width: .fractionalWidth(0.5), height: .fractionalHeight(1), spacing: 1)
+        let item = CompositionalLayout.createItem(width: .fractionalWidth(0.02), height: .fractionalHeight(0.02), spacing: 1)
         
-        let fullItem = CompositionalLayout.createItem(width: .fractionalWidth(1), height: .fractionalHeight(1), spacing: 1)
+        let fullItem = CompositionalLayout.createItem(width: .absolute(200), height: .absolute(200), spacing: 1)
         let verticalGroup = CompositionalLayout.createGroup(alignment: .vertical, width: .fractionalWidth(0.5), height: .fractionalHeight(1), item: fullItem, count: 2)
         
         let horizontalGroup = CompositionalLayout.createGroup(alignment: .horizontal, width: .fractionalWidth(1), height: .fractionalHeight(0.6), items: [item, verticalGroup])
@@ -35,13 +36,63 @@ class ViewController: UIViewController, UICollectionViewDelegate {
         // return
         return UICollectionViewCompositionalLayout(section: section)
     }
+    
+    private func layout1() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.25), heightDimension: .fractionalWidth(0.25))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.15))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        
+        return section
+    }
+    
+    private func layout2() -> NSCollectionLayoutSection {
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(200))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+            let section = NSCollectionLayoutSection(group: group)
+            section.orthogonalScrollingBehavior = .paging
+            
+            return section
+        }
+    
+    private func layout3() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalHeight(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(0.4))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .none
+                
+                return section
+            }
+    
+    func setupLayout() {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, env in
+            switch sectionIndex {
+            case 0:
+                return self.layout1()
+            case 1:
+                return self.layout2()
+            default:
+                return self.layout3()
+            }
+        }
+        collectionView.setCollectionViewLayout(layout, animated: true)
+    }
 
 }
 
 
 extension ViewController: UICollectionViewDataSource{
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return 3
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
@@ -50,6 +101,14 @@ extension ViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionCell
         cell.myImage.image = UIImage(named: images[indexPath.row])
+        switch indexPath.section {
+        case 0:
+            cell.myImage.layer.cornerRadius = cell.frame.height/2
+        case 1:
+            cell.myImage.layer.cornerRadius = 5
+        default:
+            cell.myImage.layer.cornerRadius = 5
+        }
         return cell
     }
     
